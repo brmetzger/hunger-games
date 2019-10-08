a303c752daffd0738cffba46f805ed1d4ba72087
gitrog <37446909+gitrous@users.noreply.github.com>
2019-10-08 15:39:30 -0400

    Setup command handler

---
+++ src/cmds/volunteer.js
@@ -0,0 +1,1 @@
+exports.PermissionLevel = 0;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ const DISCORD = require("discord.js");
     global.client = new DISCORD.Client();
 
 //Require internal scripts
-const CONFIG = require("./data/client.json");
+const CONFIG = require("./config.json");
 const CMDSHANDLER = require("./cmds.js");
+const SECRET = require("./data/secret.json");
 
 //Bot started
 client.on("ready",() => {
     console.log("Hunger Games bot successfully started!");
 });
 
+//Message sent
+client.on("message", message => {
+    if (message.content.startsWith("!")) {
+        CMDSHANDLER.Scan(message);
+    };
+});
+
 //Login to the bot
-client.login(CONFIG.token);
+client.login(SECRET.token);
